47e38d6ed545dcf2830987d33e72a3b2d41364a9
team-meio <phamhuyentrang562003@gmail.com>
2025-08-26 23:16:14 +0700

    introduce activity api

---
+++ src/apis/activity.ts
@@ -0,0 +1,28 @@
+
+import axiosInstance from "../config/axiosConfig";
+import { Activity } from "../interface/activity.interface";
+import { PageResponseDto } from "../interface/pagination.inerface";
+
+export const getActivities = async (params: any): Promise<PageResponseDto<Activity>> => {
+  const response = await axiosInstance.get<PageResponseDto<Activity>>("/private/v1/activities", { params });
+  return response.data;
+};
+
+export const getActivityById = async (id: string): Promise<Activity> => {
+  const response = await axiosInstance.get<Activity>(`/private/v1/activities/${id}`);
+  return response.data;
+};
+
+export const createActivity = async (data: any): Promise<Activity> => {
+  const response = await axiosInstance.post<Activity>("/private/v1/activities", data);
+  return response.data;
+};
+
+export const updateActivity = async (id: string, data: any): Promise<Activity> => {
+  const response = await axiosInstance.put<Activity>(`/private/v1/activities/${id}`, data);
+  return response.data;
+};
+
+export const deleteActivity = async (id: string): Promise<void> => {
+  await axiosInstance.delete(`/private/v1/activities/${id}`);
+};
---
+++ src/interface/activity.interface.ts
@@ -0,0 +1,10 @@
+
+export interface Activity {
+  id: string;
+  lessonId: string;
+  type: string;
+  content: any;
+  displayOrder: number;
+  createdAt: string;
+  updatedAt: string;
+}
