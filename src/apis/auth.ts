3c23d05b8f7186651d7fdb004b198d176270966f
team-meio <phamhuyentrang562003@gmail.com>
2025-09-18 13:41:41 +0700

    thêm api login parent và lấy thông tin đăng nhập

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,10 +1,16 @@
 
 import { ApiResponse } from "@/interface/base-response.interface";
+import { User } from "@/interface/user.interface";
 import axiosInstance from "../config/axiosConfig";
 import { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } from "../interface/auth.interface";
 
 export const login = async (data: LoginRequest): Promise<ApiResponse<LoginResponse>> => {
   const response = await axiosInstance.post<ApiResponse<LoginResponse>>("/public/v1/auth/admin-login", data);
+  return response.data;
+};
+
+export const parentLogin = async (data: LoginRequest): Promise<ApiResponse<LoginResponse>> => {
+  const response = await axiosInstance.post<ApiResponse<LoginResponse>>("/public/v1/auth/parent-login", data);
   return response.data;
 };
 
@@ -15,4 +21,9 @@ export const register = async (data: RegisterRequest): Promise<RegisterResponse>
 
 export const logout = async (): Promise<void> => {
   await axiosInstance.post("/public/v1/auth/logout");
+};
+
+export const getMe = async (): Promise<ApiResponse<User>> => {
+  const response = await axiosInstance.get<ApiResponse<User>>("/private/v1/auth/me");
+  return response.data;
 };
